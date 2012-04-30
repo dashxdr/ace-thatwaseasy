85ac59f610b3ef931d99c40dc3af0036a8463b0f
dashxdr <dashxdr@f4e2c25d-a5cc-8d45-4ca0-e67c0d36972f>
2012-04-30 17:43:05 +0000

    Added menu functionality, help + about options. The "about" invokes browsing to a url.

--- src/thatwaseasy/mainActivity.java
+++ src/thatwaseasy/mainActivity.java
@@ -13,6 +13,10 @@ import java.lang.System;
 import java.util.Properties;
 import java.util.Set;
 import java.util.Iterator;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.net.Uri;
 
 public class mainActivity extends Activity
 {
@@ -56,4 +60,35 @@ Log.d(TAG, "p.y = " + p.y);
 Log.d(TAG, "refreshrate = " + d.getRefreshRate());
 
 	}
+
+	private static final int ITEM_TEST_0 = 0;
+	private static final int ITEM_TEST_1 = 1;
+@Override
+	 public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add(Menu.NONE, ITEM_TEST_0, 0, "Help");
+		menu.add(Menu.NONE, ITEM_TEST_1, 0, "About");
+//		menu.add(Menu.NONE, ITEM_VISIT_IMDB, 0,
+//			getString(R.string.visit_imdb)).setIcon(android.R.drawable.ic_menu_set_as);
+//		menu.add(Menu.NONE, ITEM_VIEW_FULL_IMAGE, 0,
+//			getString(R.string.view_full_image)).setIcon(android.R.drawable.ic_menu_zoom);
+		return super.onCreateOptionsMenu(menu);
+	 }
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case ITEM_TEST_0:
+				Log.d(TAG, "menu 0 pressed");
+				return true;
+			case ITEM_TEST_1:
+				Log.d(TAG, "menu 1 pressed");
+				String abouturl = "http://www.xdr.com/acedash";
+				Intent aboutintent = new Intent(Intent.ACTION_VIEW, Uri.parse(abouturl));
+				startActivity(aboutintent);
+				return true;
+		}
+		return false;
+	}
+
+
 }
